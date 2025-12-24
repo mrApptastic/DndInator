7c1d34ac2b96137463788190f171956da0c2a029
mrApptastic <henrikbeske@gmail.com>
2025-12-24 13:24:11 +0100

    Character Generation Basics

--- DndShared/Models/CharacterStats.cs
+++ DndShared/Models/CharacterStats.cs
@@ -8,5 +8,24 @@ namespace DndShared.Models
         public int Intelligence { get; set; }
         public int Wisdom { get; set; }
         public int Charisma { get; set; }
+
+        // Helper methods for calculating ability modifiers
+        public int GetStrengthModifier() => GetAbilityModifier(Strength);
+        public int GetDexterityModifier() => GetAbilityModifier(Dexterity);
+        public int GetConstitutionModifier() => GetAbilityModifier(Constitution);
+        public int GetIntelligenceModifier() => GetAbilityModifier(Intelligence);
+        public int GetWisdomModifier() => GetAbilityModifier(Wisdom);
+        public int GetCharismaModifier() => GetAbilityModifier(Charisma);
+
+        public static int GetAbilityModifier(int abilityScore)
+        {
+            return (abilityScore - 10) / 2;
+        }
+
+        public string GetModifierString(int abilityScore)
+        {
+            int modifier = GetAbilityModifier(abilityScore);
+            return modifier >= 0 ? $"+{modifier}" : modifier.ToString();
+        }
     }
 }
