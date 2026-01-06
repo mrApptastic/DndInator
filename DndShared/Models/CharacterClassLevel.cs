78720730b8a0a4b8603c35a3d9864acaeeb72198
mrApptastic <henrikbeske@gmail.com>
2026-01-06 14:37:00 +0100

    Subclasses and Step fix for CharacterBuilder.

--- DndShared/Models/CharacterClassLevel.cs
+++ DndShared/Models/CharacterClassLevel.cs
@@ -7,5 +7,6 @@ public class CharacterClassLevel
 {
     public CharacterClass? Class { get; set; } = null;
     public int Level { get; set; } = 1;
+    public Subclass? Subclass { get; set; } = null;
     public List<string>? ClassFeatures { get; set; } = new List<string>();
 }
